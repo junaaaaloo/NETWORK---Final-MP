c2dac680d48e1850e1ecb1baa2f385a9609f63bc
ACBatosalem <angelika_batosalem@dlsu.edu.ph>
2017-03-20 17:54:37 +0800

    display cards

--- src/uno_view.py
+++ src/uno_view.py
@@ -1,13 +1,52 @@
 from tkinter import *
 
-class Server (Tk):
+
+class Main (Tk):
     def __init__ (self):
         Tk.__init__(self)
         #self.wm_iconbitmap("") #for icons
         self.title("KWATRO!")
-        self.geometry("600x600+300+0")
+        self.geometry("945x600+300+0")
         self.resizable(0, 0)
+        
+        frame2 = Frame(self, relief = RAISED, borderwidth = 30)
+        frame2.pack(fill = X)
+        buttonCards = []
+            
 
+        listCards = []
+        self.first = PhotoImage(file = 'C:\\Users\\Angel\\Documents\\2nd year - 2nd Term\\NETWORK\\NETWORK_S19-MP\\cards\\red_0.jpg')
+        self.first = self.first.subsample(2,2)
+        listCards.append(self.first)
+        self.sec = PhotoImage(file = 'C:\\Users\\Angel\\Documents\\2nd year - 2nd Term\\NETWORK\\NETWORK_S19-MP\\cards\\red_1.jpg')
+        self.sec = self.sec.subsample(2,2)
+        listCards.append(self.sec)
+        self.third = PhotoImage(file = 'C:\\Users\\Angel\\Documents\\2nd year - 2nd Term\\NETWORK\\NETWORK_S19-MP\\cards\\blue_0.jpg')
+        self.third = self.third.subsample(2,2)
+        listCards.append(self.third)
+        self.fourth = PhotoImage(file = 'C:\\Users\\Angel\\Documents\\2nd year - 2nd Term\\NETWORK\\NETWORK_S19-MP\\cards\\draw_4.jpg')
+        self.fourth = self.fourth.subsample(2,2)
+        listCards.append(self.fourth)
+        self.fifth = PhotoImage(file = 'C:\\Users\\Angel\\Documents\\2nd year - 2nd Term\\NETWORK\\NETWORK_S19-MP\\cards\\green_7.jpg')
+        self.fifth = self.fifth.subsample(2,2)
+        listCards.append(self.fifth)
+        self.sixth = PhotoImage(file = 'C:\\Users\\Angel\\Documents\\2nd year - 2nd Term\\NETWORK\\NETWORK_S19-MP\\cards\\yellow_skip.jpg')
+        self.sixth = self.sixth.subsample(2,2)
+        listCards.append(self.sixth)
+        self.seven = PhotoImage(file = 'C:\\Users\\Angel\\Documents\\2nd year - 2nd Term\\NETWORK\\NETWORK_S19-MP\\cards\\green_plus2.jpg')
+        self.seven = self.seven.subsample(2,2)
+        listCards.append(self.seven)
 
-main = Server ()
+        x = 0
+        while x != 6:
+            buttonCards.append(Button(frame2, image = listCards[x]))
+            buttonCards[x].grid(row = 0, column = x)
+            x += 1
+            
+        
+        buttonCards.append(Button(frame2, image = listCards[x]))
+        buttonCards[x].grid(row = 1, column = 0)
+        
+
+main = Main ()
 main.mainloop()
